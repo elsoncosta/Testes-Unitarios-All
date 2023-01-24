1859ffa3855a44aa0dbfc6e54ab216d4cdf446f2
Elson Costa <elson.silva@linearsistemas.com.br>
2023-01-24 14:42:22 -0300

    atualizacoes

--- Calulcos.Console/Program.cs
+++ Calulcos.Console/Program.cs
@@ -1,4 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using calculos;
 Console.WriteLine(Math.Round(4.850, 1, MidpointRounding.ToEven));
 Console.WriteLine(Math.Round(4.850, 1, MidpointRounding.AwayFromZero));
+
+decimal valorComparar1 = ((decimal)1.3).RoundAbnt(1);
+decimal valorComparar2 = ((decimal)1.7).RoundAbnt(1);
+decimal valorComparar3 = ((decimal)4.8).RoundAbnt(1);
+decimal valorComparar4 = ((decimal)4.6).RoundAbnt(1);
+decimal valorComparar5 = ((decimal)4.9).RoundAbnt(1);
+
+decimal valor1 = ((decimal)1.333);
+decimal valor2 = ((decimal)1.666);
+decimal valor3 = ((decimal)4.850);
+decimal valor4 = ((decimal)4.550);
+decimal valor5 = ((decimal)4.850);
+
+
+Console.WriteLine($"Valor Original: {valor1} : Valor Arredon.: {valor1.RoundAbnt(1)} : espera-se: {valorComparar1} : {valorComparar1.Equals(valor1.RoundAbnt(1))}");
+Console.WriteLine($"Valor Original: {valor2} : Valor Arredon.: {valor2.RoundAbnt(1)} : espera-se: {valorComparar2} : {valorComparar2.Equals(valor2.RoundAbnt(1))}");
+Console.WriteLine($"Valor Original: {valor3} : Valor Arredon.: {valor3.RoundAbnt(1)} : espera-se: {valorComparar3} : {valorComparar3.Equals(valor3.RoundAbnt(1))}");
+Console.WriteLine($"Valor Original: {valor4} : Valor Arredon.: {valor4.RoundAbnt(1)} : espera-se: {valorComparar4} : {valorComparar4.Equals(valor4.RoundAbnt(1))}");
+Console.WriteLine($"Valor Original: {valor5} : Valor Arredon.: {valor5.RoundAbnt(1)} : espera-se: {valorComparar5} : {valorComparar5.Equals(valor5.RoundAbnt(1))}");
+
 
--- calculos/DecimalExtensions.cs
+++ calculos/DecimalExtensions.cs
@@ -3,7 +3,10 @@ namespace calculos
     public static class DecimalExtensions
     {
         /// <summary>
-        /// Método de arredondamento "round-half-even
+        /// Método de arredondamento "ToEven: A estratégia de arredondar
+        /// para o número mais próximo e, quando um número estiver no meio
+        /// do caminho entre outros dois, ela é arredondada em direção ao
+        /// número par mais próximo."
         /// </summary>
         /// <param name="value"></param>
         /// <returns></returns>
@@ -12,10 +15,15 @@ namespace calculos
             return Math.Round(value, 2, MidpointRounding.ToEven);
         }
         /// <summary>
-        /// Método de arredondamento "round-half-even
+        /// Método de arredondamento "ToEven: A estratégia de arredondar
+        /// para o número mais próximo e, quando um número estiver no meio
+        /// do caminho entre outros dois, ela é arredondada em direção ao
+        /// número par mais próximo."
         /// </summary>
         /// <param name="value"></param>
-        /// <param name="precision"></param>
+        /// <param name="precision">Arredonda um valor decimal para um número
+        /// especificado de dígitos fracionários usando a convenção de
+        /// arredondamento especificada.</param>
         /// <returns></returns>
         public static decimal RoundAbnt(this decimal value, int precision)
         {
